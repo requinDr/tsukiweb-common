973e8ea5ef1fa081a38b304463b5d6fee872b51a
Raphaël Margueron <raphael.margueron@gmail.com>
2024-07-28 18:20:32 +0200

    Use range input instead of select for the text speed

--- src/constants.ts
+++ src/constants.ts
@@ -6,9 +6,12 @@ export enum ViewRatio {
   sixteenByNine = "16/9",
 }
 
-export enum TEXT_SPEED {
-  instant = 0,
-  fast = 1,
-  normal = 20,
-  slow = 50,
+export const TEXT_SPEED_MAX = 3000
+export const TEXT_SPEED_STEP_WIDTH = TEXT_SPEED_MAX / 30 // to always have 30 steps like the other settings
+
+export function TextSettingsToCharacterDelay(xi: number) {
+  xi = 1 - xi / TEXT_SPEED_MAX; // normalize input between 1 and 0
+  const slowest = 50;
+  const zoom = 0.18; // zoom on the exponent function (steepness of the curve)
+  return (((slowest * zoom + 1) ** xi) - 1) / zoom
 }
