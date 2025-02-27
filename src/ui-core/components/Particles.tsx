ad680d05d300ad7c2d5bd78ece7c0fe29108105d
requinDr <75610214+requinDr@users.noreply.github.com>
2025-02-27 21:49:39 +0100

    tabs

--- src/ui-core/components/Particles.tsx
+++ src/ui-core/components/Particles.tsx
@@ -2,15 +2,15 @@ import { memo } from "react"
 import styles from "../styles/particles.module.scss"
 
 const Particles = () => {
-  return (
-    <div className={styles.particles}>
-      {Array.from({ length: 100 }, (_, index) =>
-        <div className={styles.circleContainer} key={index}>
-          <div className={styles.circle} />
-        </div>
-      )}
-    </div>
-  )
+	return (
+		<div className={styles.particles}>
+			{Array.from({ length: 100 }, (_, index) =>
+				<div className={styles.circleContainer} key={index}>
+					<div className={styles.circle} />
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default memo(Particles)
