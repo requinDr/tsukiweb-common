34e9e5c75cf9b7a77b319b63232e59938b637ce1
requinDr <75610214+requinDr@users.noreply.github.com>
2025-03-11 21:32:33 +0100

    Global let supportAvif

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,13 +1,25 @@
+export let supportAvif: boolean | null = null
+
 export async function testAvifSupport(): Promise<boolean> {
+	if (supportAvif !== null)
+		return supportAvif
+
 	return new Promise(resolve => {
 		const img = new Image()
-		img.onload = () => resolve(true)
-		img.onerror = () => resolve(false)
+		img.onload = () => {
+			supportAvif = true
+			resolve(true)
+		}
+		img.onerror = () => {
+			supportAvif = false
+			resolve(false)
+		}
 		
 		// base64 AVIF header
 		img.src = 'data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=';
 	})
 }
+testAvifSupport()
 
 export const replaceExtensionByAvif = (src: string): string => {
 	if (src.endsWith(".webp"))
