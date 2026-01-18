5738dbc51fc81c41a67474e36afce5b4a487bdc5
requinDr <75610214+requinDr@users.noreply.github.com>
2026-01-18 09:41:49 +0100

    Allow props on footer

--- src/ui-core/components/FixedFooter.tsx
+++ src/ui-core/components/FixedFooter.tsx
@@ -1,11 +1,12 @@
+import { ComponentProps } from 'react';
 import styles from '../styles/fixedfooter.module.scss';
 
 type Props = {
 	children?: React.ReactNode
-}
-const FixedFooter = ({ children }: Props) => {
+} & ComponentProps<'footer'>
+const FixedFooter = ({ children, className, ...props }: Props) => {
 	return (
-		<footer className={styles.footer}>
+		<footer className={`${styles.footer} ${className || ''}`} {...props}>
 			<div className={styles.footerContent}>
 				{children}
 			</div>
