d78b105f92aa6936a592188adf6c148a337b33a5
requinDr <75610214+requinDr@users.noreply.github.com>
2026-07-11 17:46:12 +0200

    Remove GameAudioManager

--- src/audio/AudioManager.ts
+++ src/audio/AudioManager.ts
@@ -3,9 +3,6 @@ import { asyncDelay } from "../utils/timer"
 import { AudioSourceNode, Sound} from "./AudioSourceNode"
 import { StreamingAudioNode } from "./StreamingAudioNode"
 import { AutoMuteAudioContext } from "./AutoMuteContext"
-import { settings } from "engine/settings";
-import { observe } from "../utils/Observer";
-import { calcGain } from "./utils";
 
 const effects: Record<string, Sound> = {
     'glass' : {
@@ -264,42 +261,3 @@ export class AudioManager {
         }
     }
 }
-
-//#endregion ###################################################################
-//#region                       GameAudio
-//##############################################################################
-
-export class GameAudioManager extends AudioManager {
-    _inGame: boolean = false
-
-    constructor (...params: ConstructorParameters<typeof AudioManager>) {
-        super(...params)
-        this._updateVolumes = this._updateVolumes.bind(this)
-        this._updateVolumes()
-        for (const attr of ['master', 'se', 'titleTrack', 'systemSE', 'track'] as const)
-            observe(settings.volume, attr, this._updateVolumes)
-        observe(settings, 'autoMute', (m) => { this.autoMute = m })
-        this.autoMute = settings.autoMute
-    }
-    
-    private _updateVolumes() {
-        this.masterVolume = calcGain(settings.volume.master)
-        this.uiVolume = calcGain(settings.volume.systemSE)
-        this.waveVolume = calcGain(settings.volume.se)
-        this.trackVolume = calcGain(
-            (this._inGame) ? settings.volume.track
-                           : settings.volume.titleTrack)
-    }
-
-    get inGame() { return this._inGame }
-    set inGame(inGame: boolean) {
-        if (inGame != this._inGame) {
-            this._inGame = inGame
-            this.stopTrack()
-            this.stopWave()
-            this._updateVolumes()
-        }
-    }
-}
-
-//#endregion ###################################################################
